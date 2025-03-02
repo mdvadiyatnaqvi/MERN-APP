d5c2a84215c7343ca59505ffc20c3d4088a560c5
Md Vadiyat Naqvi <mdvadiyatnaqvi@gmail.com>
2025-03-02 15:13:02 +0530

    model for user created

---
+++ server/models/user-model.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    typeof: "string",
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: "String",
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: "String",
+    required: true,
+  },
+  password: {
+    type: "String",
+    required: true,
+  },
+  isAdmin: {
+    type: "Boolean",
+    default: false,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
