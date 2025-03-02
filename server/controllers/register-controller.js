eba20174297eedc0af77f9c35d5d4c0cdf528b2f
Md Vadiyat Naqvi <mdvadiyatnaqvi@gmail.com>
2025-03-02 12:57:00 +0530

    code updated

--- server/controllers/register-controller.js
+++ server/controllers/register-controller.js
@@ -1,7 +1,7 @@
 const register = async (req, res) => {
   try {
     res.status(200).json({ message: req.body });
-    console.log(req.body);
+    // console.log(req.body);
   } catch (error) {
     res.status(404).json("Internal Server Error");
   }
--- server/utils/db.js
+++ server/utils/db.js
@@ -5,7 +5,7 @@ const URI = "mongodb://127.0.0.1:27017/mern_admin";
 const connectDB = async () => {
   try {
     await mongoose.connect(URI);
-    console.log("MongoDB connected!");
+    console.log("Database connected!");
   } catch (error) {
     console.error("Database connection failed:", error.message);
     process.exit(1); // Exit with a non-zero status code to indicate failure
