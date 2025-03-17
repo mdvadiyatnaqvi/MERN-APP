0a8e17f741c18c2a1e6f8e8c22cc5f36c84acc40
Md Vadiyat Naqvi <mdvadiyatnaqvi@gmail.com>
2025-03-17 10:03:45 +0530

    now user can access api from any ip

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const authRoutes = require("./router/auth-router");
 const contactRoute = require("./router/contact-router");
 const connectDb = require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware");
 
 const app = express();
+app.use(cors);
 app.use(express.json());
 app.use(authRoutes);
 app.use(contactRoute);
